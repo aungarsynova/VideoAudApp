ce78f27ff3a83d6032815dc58a575cdc9a672f32
unknown <you@example.com>
2019-03-14 21:39:43 -0400

    added database in db folder and some comments

--- js/modules/UserComponent.js
+++ js/modules/UserComponent.js
@@ -1,16 +1,19 @@
 export default {
     props: ['liveuser'],
 
+    //Caitlin, this is template.
+    //Put your HTML for UserComponent in here. DO not delete back ticks -> `
+    //This is Trevor's template, use it as an example to uderstand HTML
     template: `
-        <div class="col-xs-12 col-sm-6 col-md-4 mx-auto">
-            <div class="card rounded" @click="navToUserHome()">
-                <div class="card-body text-center">
-                    <!-- <pre>{{ liv euser }}</pre> -->
-                    <img :src="'images/' + liveuser.avatar" class="rounded-circle img-fluid">
-                    <p>{{liveuser.username}}</p>
-                </div>
-            </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 mx-auto">
+    <div class="card rounded" @click="navToUserHome()">
+        <div class="card-body text-center">
+            <!-- <pre>{{ liveuser }}</pre> -->
+            <img :src="'images/' + liveuser.avatar" class="rounded-circle img-fluid">
+            <p>{{liveuser.username}}</p>
         </div>
+    </div>
+</div>
     `,
 
     created: function() {
--- js/modules/UserHomeComponent.js
+++ js/modules/UserHomeComponent.js
@@ -1,18 +1,20 @@
 export default {
     props: ['liveuser'],
 
+    //Caitlin, this is template.
+    //Put your HTML for usersHomeComponent in here. DO not delete back ticks -> `
     template: `
-    <div class="container"
-        <div class="row">
-            <div class="col-sm-12">
-                <h1>You're (finally) on the user's page!</h1>
-            </div>
+    <div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+        <h1 class="user-message text-center">{{message}}</h1>
         </div>
-    </div>
     `,
 
     data(){
         return {
+            //Caitlin, delete message after editing the template
+            message: "This is Users Home Component. A main page where videos and info from database is shown. Add HTML inside the template in order for the data to be displayed on this page",
             activeMediaType: "type",
 
             //push first result and push it into an active media referece
